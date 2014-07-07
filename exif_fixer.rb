0a3f6d7c70580311343c62bdeab0c09aaa361c93
th3james <th3james@fastmail.fm>
2014-07-07 12:18:26 +0100

    Make script run (albeit, totally wrong)

--- exif_fixer.rb
+++ exif_fixer.rb
@@ -1,4 +1,10 @@
-Photos.from_directory.each do |photo|
+class Photo
+  def self.from_directory
+    []
+  end
+end
+
+Photo.from_directory.each do |photo|
   timestamp = photo.get_non_standard_timestamp
   photo.set_created_at(timestamp)
 end
