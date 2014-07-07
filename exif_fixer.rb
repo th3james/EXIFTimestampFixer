f7a355ea5e98ddd12b3bd056bd5cdffa9f221ba2
th3james <th3james@fastmail.fm>
2014-07-07 12:56:15 +0100

    Re-write specs to use expect and subject

--- exif_fixer.rb
+++ exif_fixer.rb
@@ -1,10 +1,26 @@
 class Photo
-  def self.from_directory
+  def self.from_directory directory_path
     []
   end
 end
 
-Photo.from_directory.each do |photo|
+describe Photo, "#from_directory" do
+  context "given the test asset directory" do
+    subject(:photos) { Photo.from_directory('test_assets') }
+
+    it "returns the an array of photos in the given directory" do
+      expect(photos.length).to eq(3)
+    end
+
+    it "returns Photo instances" do
+      expect(photos[0]).to be_a(Photo)
+    end
+  end
+end
+
+photo_dir = gets("What directory are the photos in?")
+
+Photo.from_directory(photo_dir).each do |photo|
   timestamp = photo.get_non_standard_timestamp
   photo.set_created_at(timestamp)
 end
